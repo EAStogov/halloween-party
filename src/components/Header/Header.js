da2b110ac122dd5e5d60fd63c566acdca30c4773
Egor Stogov <egst94@yandex.ru>
2023-09-25 12:33:57 +0300

    feature: add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return(
+    <section className='header'>
+      <h1 className='header__title'>HALLOWEEN</h1>
+      <h2 className='header__subtitle'>PARTY</h2>
+    </section>
+  );
+}
+
+export default Header;
