3d5aac002e378206ab7ae18f4e84d501ba77843e
Egor Stogov <egst94@yandex.ru>
2023-09-25 11:59:47 +0300

    docs: add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,130 @@
+export default imageList = [
+  {
+    url: '../images/image1',
+    alt: '#'
+  },
+  {
+    url: '../images/image2',
+    alt: '#'
+  },
+  {
+    url: '../images/image3',
+    alt: '#'
+  },
+  {
+    url: '../images/image4',
+    alt: '#'
+  },
+  {
+    url: '../images/image5',
+    alt: '#'
+  },
+  {
+    url: '../images/image6',
+    alt: '#'
+  },
+  {
+    url: '../images/image7',
+    alt: '#'
+  },
+  {
+    url: '../images/image8',
+    alt: '#'
+  },
+  {
+    url: '../images/image9',
+    alt: '#'
+  },
+  {
+    url: '../images/image10',
+    alt: '#'
+  },
+  {
+    url: '../images/image11',
+    alt: '#'
+  },
+  {
+    url: '../images/image12',
+    alt: '#'
+  },
+  {
+    url: '../images/image13',
+    alt: '#'
+  },
+  {
+    url: '../images/image14',
+    alt: '#'
+  },
+  {
+    url: '../images/image15',
+    alt: '#'
+  },
+  {
+    url: '../images/image16',
+    alt: '#'
+  },
+  {
+    url: '../images/image17',
+    alt: '#'
+  },
+  {
+    url: '../images/image18',
+    alt: '#'
+  },
+  {
+    url: '../images/image19',
+    alt: '#'
+  },
+  {
+    url: '../images/image21',
+    alt: '#'
+  },
+  {
+    url: '../images/image22',
+    alt: '#'
+  },
+  {
+    url: '../images/image23',
+    alt: '#'
+  },
+  {
+    url: '../images/image24',
+    alt: '#'
+  },
+  {
+    url: '../images/image25',
+    alt: '#'
+  },
+  {
+    url: '../images/image26',
+    alt: '#'
+  },
+  {
+    url: '../images/image27',
+    alt: '#'
+  },
+  {
+    url: '../images/image28',
+    alt: '#'
+  },
+  {
+    url: '../images/image29',
+    alt: '#'
+  },
+  {
+    url: '../images/image30',
+    alt: '#'
+  },
+  {
+    url: '../images/image31',
+    alt: '#'
+  },
+  {
+    url: '../images/image32',
+    alt: '#'
+  },
+  {
+    url: '../images/image33',
+    alt: '#'
+  }
+]
